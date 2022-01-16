a4393cfb877165d683b33324f83aa5c2931589bd
Zach McAdams <zlmg2b@umsystem.edu>
2022-01-16 09:42:52 -0600

    Add notes about cutadapt failure.

---
+++ code/README.md
@@ -0,0 +1,7 @@
+# Notes for code development
+###### 1/16/22
+When builidng PCoA plots for human samples, found that no human samples
+had passed the trimming step. Assuming adpater/primer sequence input differs
+between the mouse/human data sets. 
+
+Reached out to collaborators for new input sequences.
--- code/pcoa_plots.R
+++ code/pcoa_plots.R
@@ -20,9 +20,46 @@ library(vegan)
 ## Goals:
 ## -- Plot PCoAs for human/mouse studies
 
-build_pcoa <- function(pcoa_results) {
+build_pcoa_mouse <- function(pcoa_results) {
   
-  hull <- jaccard_pcoa$data$Vectors %>%
+  hull <- pcoa_results$data$Vectors %>%
+    select(SampleID, PC1, PC2) %>% 
+    inner_join(., metadata, by = "SampleID") %>% 
+    filter(Source == "Mouse") %>% 
+    # select relevant columns, leaving out Eartag
+    select(SampleID, PC1, PC2, Day, Dilution) %>% 
+    filter(Day == 28) %>% 
+    group_by(Dilution) %>%
+    slice(chull(PC1, PC2))
+  
+  
+  pcoa_results$data$Vectors %>%
+    select(SampleID, PC1, PC2) %>% 
+    left_join(metadata) %>% 
+    filter(Day == 28) %>% 
+    ggplot(aes(x=PC1, y=PC2, color = Dilution, shape = Dilution)) +
+    geom_point(alpha=0.6, size = 2) + #alpha controls transparency and helps when points are overlapping
+    geom_polygon(data = hull,
+                 aes(fill = Dilution,
+                     color = Dilution),
+                 alpha = 0.3,
+                 show.legend = FALSE) +
+    labs(x = c(paste("PCo1 - ",round(pcoa_results$data$Eigvals$PC1, 2), "%", sep = "")),
+         y = c(paste("PCo2 - ",round(pcoa_results$data$Eigvals$PC2, 2), "%", sep = ""))) +
+    theme_q2r() +
+    theme(axis.text = element_text(color = "black", size = 10,
+                                   family = "sans", face = "bold"),
+          axis.title = element_text(color = "black", size = 12, 
+                                    family = "sans", face = "bold"),
+          legend.text = element_text(color = "black", size = 8,
+                                     family = "sans", face = "bold"),
+          legend.title = element_text(color = "black", size = 10,
+                                      family = "sans", face = "bold")) 
+}
+
+build_pcoa_human <- function(pcoa_results) {
+  
+  hull <- pcoa_results$data$Vectors %>%
     select(SampleID, PC1, PC2) %>% 
     inner_join(., metadata, by = "SampleID") %>% 
     filter(Source == "Mouse") %>% 
@@ -64,23 +101,43 @@ getwd()
 metadata <- read_tsv("metadata.txt")
 
 jaccard_pcoa <- read_qza("core-metrics-results_42487/jaccard_pcoa_results.qza")
-j_pcoa <- build_pcoa(jaccard_pcoa) + ggtitle("Jaccard")
-j_pcoa
+j_pcoa_mouse <- build_pcoa_mouse(jaccard_pcoa) + ggtitle("Jaccard")
 
-jaccard_dist <- read_qza("core-metrics-results_42487/jaccard_distance_matrix.qza")
-jaccard_dist$data
-bc_pcoa <- read_qza("mk_study_A/core-metrics-results_10146/bray_curtis_pcoa_results.qza")
-bc_pcoa <- build_pcoa(bc_pcoa)
+bc_pcoa <- read_qza("core-metrics-results_42487/bray_curtis_pcoa_results.qza")
+bc_pcoa_mouse <- build_pcoa_mouse(bc_pcoa) + ggtitle("Bray Curtis")
 
 
+## Plot mouse data
+leg <- get_legend(j_pcoa_mouse)
 
-
-
-leg <- get_legend(j_pcoa)
-prow <- plot_grid(j_pcoa + theme(legend.position = "none"),
-                  bc_pcoa + theme(legend.position = "none"),
+prow <- plot_grid(j_pcoa_mouse + theme(legend.position = "none",
+                                 title = element_text(color = "black",
+                                                      face = "bold")),
+                  bc_pcoa_mouse + theme(legend.position = "none",
+                                  title = element_text(color = "black",
+                                                       face = "bold")),
                   labels = "AUTO",
                   hjust = 0)
 
-plot_grid(prow, leg, rel_widths = c(3,0.8))
+plot_grid(prow, leg, rel_widths = c(3,0.3))
+
+ggsave("plots/pcoa/mouse_d28_bc_j_pcoa_no-stats.tiff", 
+       dpi = 600, bg = "white")
+
+
+metadata <- read_tsv("metadata.txt")
+
+
+jaccard_pcoa$data$Vectors %>%
+  select(SampleID, PC1, PC2) 
+  inner_join(., metadata, by = "SampleID") %>% 
+  count(Source)
+  # select relevant columns, leaving out Eartag
+  select(SampleID, PC1, PC2, Day, Dilution) %>% 
+  filter(Day == 28) %>% 
+  group_by(Dilution) %>%
+  slice(chull(PC1, PC2))
+
+
+
 
